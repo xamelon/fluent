32db5ffe1c16fe15e8ea89d16be7e4c75e5922d9
Stas Buldakov <xamelon96@gmail.com>
2021-01-28 15:29:19 +0400

    effecthandler

--- lib/src/fluent.dart
+++ lib/src/fluent.dart
@@ -5,7 +5,7 @@ import 'package:meta/meta.dart';
 import 'dart:async';
 import './map.dart';
 
-typedef Map<String, dynamic> EffectHandler(Map<String, dynamic> state, [dynamic args]);
+typedef FutureOr<Map<String, dynamic>> EffectHandler(Map<String, dynamic> state, [dynamic args]);
 
 class Fluent {
   static final Fluent fluent = Fluent._();
@@ -16,50 +16,56 @@ class Fluent {
 
   Map<String, dynamic> state = {};
   
-  Map<String, List<EffectHandler>> _effects = {};
+  Map<String, EffectHandler> _effects = {};
 
   Map<String, StreamController> _subs = {};
 
   List<Map<String, Map>> hx = List.empty(growable: true);
 
-  static void dispatch(String eventName, List args) {
-    List<EffectHandler> mappings = fluent._effects[eventName];
-    if (mappings == null) {
+  static void dispatch(String eventName, List args) async {
+    EffectHandler handler = fluent._effects[eventName];
+    if (handler == null) {
       return;
     }
-    mappings.forEach((m) {
-      List<dynamic> handlerArgs = [fluent.state]..addAll(args);
-      Map diffMap = Function.apply(m, handlerArgs);
-      fluent.state = diffMap;
-      List<String> allPaths = diffMap.findAllPaths();
-
-      List changedKeys =
-          allPaths.where((el) => fluent._subs.containsKey(el)).toList();
+    
+        
+    Map diffMap = await Function.apply(handler, [fluent.state]..addAll(args));
+    fluent.state = diffMap;
+    List<String> allPaths = diffMap.findAllPaths();
+    
+    List changedKeys = allPaths.where((el) => fluent._subs.containsKey(el)).toList();
+    
+    if(changedKeys != null) {
       changedKeys.forEach((el) {
-        StreamController ctrl = fluent._subs[el];
-        var val = diffMap.getIn(el);
-        ctrl.add(val);
-      });
-
+          StreamController ctrl = fluent._subs[el];
+          var val = diffMap.getIn(el);
+          
+          ctrl.add(val);
+        });
+      }
+      
       if (fluent.saveHistory) {
         fluent.hx.add({eventName: diffMap});
       }
-    });
+    
   }
 
   static void regEffect(
-      String eventName, EffectHandler handler) {
-    if (fluent._effects.containsKey(eventName)) {
-      fluent._effects[eventName].add(handler);
-    } else {
-      fluent._effects[eventName] = List<EffectHandler>()..add(handler);
-    }
+    String eventName, EffectHandler handler) {
+    fluent._effects[eventName] = handler;
   }
 
+  void _cleanSubscribers(String path) {
+    fluent._subs[path] = null;
+  }
+  
   static Stream regSub(String path) {
+    
     StreamController controller = fluent._subs[path];
     if (controller == null) {
-      controller = StreamController();
+      controller = StreamController(onCancel: () {
+          Fluent.fluent._cleanSubscribers(path);
+      });
       fluent._subs[path] = controller;
     }
 
