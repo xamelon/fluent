61d48266283c2f35a38bd7927d15a51c417e61c7
Alexander <alex@Mac-mini-Alexander.local>
2021-02-20 11:39:08 +0400

    Added "list to map" convertation function
    1. Added list convertation function to debug_list_item
    2. Auto-refactored debug_list_item, map_view, debug.

--- lib/src/debug/debug.dart
+++ lib/src/debug/debug.dart
@@ -4,13 +4,10 @@ import './map_view.dart';
 import 'package:fluent/fluent.dart';
 
 class FluentDebug extends StatefulWidget {
-
   State<FluentDebug> createState() => _FluentDebugState();
-  
 }
 
 class _FluentDebugState extends State<FluentDebug> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,5 +23,4 @@ class _FluentDebugState extends State<FluentDebug> {
       ),
     );
   }
-  
 }
--- lib/src/debug/debug_list_item.dart
+++ lib/src/debug/debug_list_item.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 
-import './map_view.dart'; 
+import './map_view.dart';
 
 class DebugListItem extends StatelessWidget {
-
   bool expanded;
   String k;
   var value;
@@ -11,24 +10,35 @@ class DebugListItem extends StatelessWidget {
   int level;
 
   DebugListItem(this.k, this.value, this.expanded, this.onPressed, this.level);
-  
+
   bool canBeExpanded() => (value is Map || value is List);
 
   Widget childForExpanded() {
-    if(expanded) {
-      if(value is Map) {
+    if (expanded) {
+      if (value is Map) {
         return MapView(value, level > 3 ? level : level++);
-      } else if(value is List) {
-        Map targetMap = value.asMap();
-        return MapView(targetMap, level > 3 ? level : level++);
+      } else if (value is List) {
+        Map _convertedMap = _convertToMap(value);
+        return MapView(_convertedMap, level > 3 ? level : level++);
       }
     }
 
     return Container();
   }
 
+  Map _convertToMap(List list) {
+    Map _result = {};
+    Map _targetMap = list.asMap();
+
+    _targetMap.forEach((key, value) {
+      _result[key.toString()] = value;
+    });
+
+    return _result;
+  }
+
   Widget wrapInGestureDetector({Widget child}) {
-    if(canBeExpanded()) {
+    if (canBeExpanded()) {
       return InkWell(
         onTap: this.onPressed,
         child: child,
@@ -36,37 +46,34 @@ class DebugListItem extends StatelessWidget {
     }
     return child;
   }
-  
+
   @override
   Widget build(BuildContext context) {
-    return 
-      Container(
-        constraints: BoxConstraints(minHeight: 30),
-        padding: EdgeInsets.only(left: 30 * level.toDouble()),
+    return Container(
+      constraints: BoxConstraints(minHeight: 30),
+      padding: EdgeInsets.only(left: 30 * level.toDouble()),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Container(),
           wrapInGestureDetector(
-            child:Row(children: [
-              if(canBeExpanded())
-              Icon(expanded ? Icons.arrow_drop_down : Icons.arrow_right),
+            child: Row(children: [
+              if (canBeExpanded())
+                Icon(expanded ? Icons.arrow_drop_down : Icons.arrow_right),
               Container(width: 5),
               Text("$k:", style: TextStyle(color: Colors.teal)),
               Container(width: 5),
-              if(!canBeExpanded())
-              Expanded(child: Text("$value")),
-        ]),),
-        childForExpanded(),
-        Container(
-          height: 1.0,
-          color: Colors.grey,
-          
-        ),
+              if (!canBeExpanded()) Expanded(child: Text("$value")),
+            ]),
+          ),
+          childForExpanded(),
+          Container(
+            height: 1.0,
+            color: Colors.grey,
+          ),
         ],
-    ),
+      ),
     );
   }
-  
 }
